ce13e6949c132972aba9b880020c47c734873cdd
Daniel <demartinezgatell@gmail.com>
2020-01-07 16:54:16 +0000

    refactoring

--- public/js/note-controller.js
+++ public/js/note-controller.js
@@ -1,14 +1,16 @@
+document.getElementById("app").innerText = "howdy";
 
-(function(exports){
-  function NoteController(noteListModel) {
-    this.noteListModel = noteListModel
-  }
-  NoteController.prototype.displayHtml = function() {
-    this.noteListModel.add("Favorite drink: seltzer")
-    noteListView = new NoteListView(this.noteListModel);
-    html = noteListView.htmlString();
-    document.getElementById("app").innerText = html;
-  }
-
-  exports.NoteController = NoteController
-})(this)
+// (function(exports){
+//   function NoteController(noteListModel) {
+//     this.noteListModel = noteListModel
+//   }
+//   NoteController.prototype.displayHtml = function() {
+//     this.noteListModel.add("Favorite drink: seltzer")
+//     noteListView = new NoteListView(this.noteListModel);
+//     console.log(noteListView);
+//     html = noteListView.htmlString();
+//     document.getElementById("app").innerText = html;
+//   }
+//
+//   exports.NoteController = NoteController
+// })(this)
