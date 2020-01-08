de88729fb199dcbf841dac6f5443218cc90bc505
Ellie <elliemmanifold@gmail.com>
2020-01-08 16:36:28 +0000

    Shorten to only show first 20 characters

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -4,7 +4,7 @@ describe('NoteController', () => {
       var noteController = new NoteController(new NoteList());
       element = { innerHTML: 'Something' }
       noteController.displayHtml(element)
-      expect(element.innerHTML).toEq('<ul><li><div>Favourite drink: seltzer</div></li></ul>')
+      expect(element.innerHTML).toEq('<ul><li><div>Favourite drink: sel</div></li></ul>')
     })
   })
 })
